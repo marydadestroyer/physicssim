a316bb47b5c4ab784331e740622f2734206b4543
Mary Harrell <harrell_mary@columbusstate.edu>
2020-05-15 20:41:44 +0000

    adding test page

---
+++ Physics_Sim_Template/Content/test.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     <link rel="stylesheet" href="Physics Sim Template/CSS/styles.css">
+    <title>Physics Sim</title>
+
+
+  </head>
+  <body>
+  <!--NavBar-->
+    <section id="nav-bar">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand text-white" href="home.php">PhysicsSim</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="home.php">HOME </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="features.php">FEATURES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="gettingStarted.php">GET STARTED</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutUs.php">ABOUT US</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="help.php">HELP</a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </section>
+
+
+    <div class="container-fluid text-center" >
+      <!-- Content here  NEED EDITS-->
+
+      <h1 class= " font-weight-bold text-center">Welcome to PhysicsSim </h1>
+
+      <p class="text-center">An Intelligent Tutoring System to Teach Kinematics</p>
+     
+      < href="https://physicssim.000webhostapp.com/PhysicsSim/webBuild/index.html">Get Started</button>
+    </div>
+
+
+
+ 
+
+
+
+
+
+
+<footer class="footer">
+  <div class="container">
+    <div class="row-align-items-center">
+      <div class="col-lg-12 text-center text-white">
+           <span>Copyright &copy; PhysicsSim 2020</span>
+        </div>
+  </div>
+</footer>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
